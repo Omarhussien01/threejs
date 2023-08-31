ef083090debc05ae5c5604acb5d0ae650fb27280
HomePC <omar.metwally231@gmail.com>
2023-08-31 23:15:28 +0300

    OpenAi bug

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,32 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import OpenAI from "openai";
+
+dotenv.config();
+
+const router = express.Router();
+
+const openai = new OpenAI();
+
+router.route("/").get((req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E ROUTES" });
+});
+
+router.route("/").post(async (req, res) => {
+  try {
+    const { prompt } = req.body;
+
+    const response = await openai.images.generate({
+      prompt: "A cute baby sea otter",
+    });
+
+    const image = response.data.data[0].b64_json;
+
+    res.status(200).json({ photo: image });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Something went wrong" });
+  }
+});
+
+export default router;
